f3f5ab6321fc09c851ada5d4fd80ac1d457842cc
Graham Kelly <gkgoat6700@gmail.com>
2026-01-04 21:05:36 -0500

    abstract

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 #![no_std]
-pub fn ribose(a: &mut [u8], mut rand: &mut (dyn FnMut(&mut u8) + '_)) {
+pub static DEFAULT_ROOT_PATHS: &[&[u8]] = &[
+    b"home/" as &[u8],
+    b"media/",
+    b"rustc/",
+    b"Users/",
+    b"Volumes/",
+];
+pub static DEFAULT_EXTENSIONS: &[&[u8]] = &[b".rs" as &[u8], b".c", b".h", b".zig"];
+pub fn ribose(
+    a: &mut [u8],
+    mut rand: &mut (dyn FnMut(&mut u8) + '_),
+    root_paths: Option<&[&[u8]]>,
+    extensions: Option<&[&[u8]]>,
+) {
+    let root_paths = root_paths.unwrap_or(DEFAULT_ROOT_PATHS);
+    let extensions = extensions.unwrap_or(DEFAULT_EXTENSIONS);
     for i in 0..(a.len()) {
         let mut censor = false;
         if a[i] == b'/' {
-            for s in [b"home/" as &[u8], b"media/", b"rustc/",b"Users/",b"Volumes/"] {
+            for s in root_paths.iter().cloned() {
                 if a[(i + 1)..][..(s.len())] == *s
                     || a[(i + 1)..][..(s.len() * 2)]
                         .iter()
@@ -25,7 +40,7 @@ pub fn ribose(a: &mut [u8], mut rand: &mut (dyn FnMut(&mut u8) + '_)) {
         if censor {
             'a: for j in i..(a.len()) {
                 rand(&mut a[j]);
-                for s in [b".rs" as &[u8], b".c", b".h", b".zig"] {
+                for s in extensions.iter().cloned() {
                     if a[j..][..(s.len())] == *s
                         || a[j..][..(s.len() * 2)]
                             .iter()
